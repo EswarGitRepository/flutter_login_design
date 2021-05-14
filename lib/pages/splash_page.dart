74c3b354bf5ead18a474bfa780c5a54986438f4c
Eswar GitHub <udvala.eswar@gmail.com>
2021-05-14 23:05:30 +0530

    firstcommit

--- lib/pages/splash_page.dart
+++ lib/pages/splash_page.dart
@@ -13,6 +13,7 @@ class SplashPage extends StatefulWidget {
 class _SplashPageState extends State<SplashPage> {
   @override
   void initState() {
+    // ignore: todo
     // TODO: implement initState
     super.initState();
     Timer(const Duration(milliseconds: 4000), () {
